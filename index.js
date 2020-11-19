7997d4746865a09855f400cbf10fd339565aff9b
Jeong-seungok <vsnm25@naver.com>
2020-11-19 17:50:27 +0900

    'version1.0.1'

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const secEl = document.querySelector('#sec');
 const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
 const clearBtn = document.querySelector('#clear');
-const showTimeEl = document.querySelector('#show-time');
-const now = new Date().toDateString();
+const cHourEl = document.querySelector('#c-hour');
+const cMinEl = document.querySelector('#c-min');
+const cSecEl = document.querySelector('#c-sec');
+let checkWork = true;
+let startOn = false;
 
 // (시)단위 선택목록 생성
-for(let i=0;i<10;i++){
+for(let i=0;i<=24;i++){
     const optionEl = document.createElement('option');
-    optionEl.value = i;
-    optionEl.innerText = i;
+    if(i<10){
+        optionEl.value = `0${i}`;
+        optionEl.innerText = `0${i}`;
+    }
+    else{
+        optionEl.value = i;
+        optionEl.innerText = i;
+    }
     hour.append(optionEl);
 }
 // (분,초)단위 선택목록 생성
@@ -20,10 +29,18 @@ for(let i=0;i<60;i++){
     const optionEl = document.createElement('option');
     const option1El = document.createElement('option');
     
-    optionEl.value = i;
-    optionEl.innerText = i;
-    option1El.value = i;
-    option1El.innerText = i;
+    if(i<10){
+        optionEl.value = `0${i}`;
+        option1El.value = `0${i}`;
+        optionEl.innerText = `0${i}`;
+        option1El.innerText = `0${i}`;
+    }
+    else{
+        optionEl.value = i;
+        option1El.value = i;
+        optionEl.innerText = i;
+        option1El.innerText = i;
+    }
     min.append(optionEl);
     sec.append(option1El);
 }
@@ -33,38 +50,65 @@ for(let i=0;i<60;i++){
 // 모두 00이 되면 종료
 // 처음 선택 / 선택하고나서 시분초 계산
 // 진행중인 시간이 있으면 start 버튼 이벤트 막기
-startBtn.addEventListener('click',(e)=>{
-    let cHour = showTimeEl.innerHTML === '' ? Number(hourEl.value) : Number(showTimeEl.innerText.split(' ')[0]);
-    let cMin = showTimeEl.innerHTML === '' ? Number(minEl.value) : Number(showTimeEl.innerText.split(' ')[1]);
-    let cSec = showTimeEl.innerHTML === '' ? Number(secEl.value) : Number(showTimeEl.innerText.split(' ')[2]);
 
-    if(showTimeEl.innerHTML !== ''){
-        alert('진행중인 시간이 있습니다');
+startBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let cHourValue = checkWork ? hourEl.value : cHourEl.innerText;
+    let cMinValue = checkWork ? minEl.value : cMinEl.innerText;
+    let cSecValue = checkWork ? secEl.value : cSecEl.innerText;
+
+    cHourEl.innerText = cHourValue;
+    cMinEl.innerText = cMinValue;
+    cSecEl.innerText = cSecValue;
+
+    if(startOn){
+        alert('이미 실행중입니다.');
         return false;
     }
-    
-    const start = setInterval(()=>{
-        showTimeEl.innerHTML= `${cHour} ${cMin} ${cSec}`;
-        if(cHour === 0 && cMin === 0 && cSec === 0){
-            alert('종료되었습니다');
-            showTimeEl.innerHTML = '';
+
+    const start =  setInterval(()=>{
+        startOn = true;
+        checkWork = false;
+        // 시,분,초 모두 00이면 종료
+        if(cHourEl.innerText === '00' && cMinEl.innerText === '00' && cSecEl.innerText === '00'){
+            alert('Time completed!!');
+            checkWork = true;
             clearInterval(start);
         }
-        else if(cMin === 0 && cSec === 0){
-            cHour -= 1;
-            cMin = 59;
-            cSec = 59;
+        // 분,초 00이면 시-1
+        else if(cMinEl.innerText === '00' && cSecEl.innerText === '00'){
+            if(Number(cHourEl.innerText)<11)
+                cHourEl.innerText = `0${Number(cHourEl.innerText)-1}`;
+            else
+                cHourEl.innerText = `${Number(cHourEl.innerText)-1}`;
+            cMinEl.innerText = '59';
+            cSecEl.innerText = '59';
         }
-        else if(cSec === 0){
-            cMin -= 1;
-            cSec = 59;
+        // 초 00이면 분-1
+        else if(cSecEl.innerText === '00'){
+            if(Number(cMinEl.innerText)<11)
+                cMinEl.innerText = `0${Number(cMinEl.innerText)-1}`;
+            else
+                cMinEl.innerText = `${Number(cMinEl.innerText)-1}`;
+            cSecEl.innerText = '59';
         }
-        cSec -= 1;
+        // 그 외 경우 초만 -1
+        else{
+            if(Number(cSecEl.innerText)<11)
+                cSecEl.innerText = `0${Number(cSecEl.innerText)-1}`;
+            else
+                cSecEl.innerText = `${Number(cSecEl.innerText)-1}`;
+        }
         pauseBtn.addEventListener('click',()=>{
+            startOn = false;
             clearInterval(start);
         });
         clearBtn.addEventListener('click',()=>{
-            showTimeEl.innerHTML = '';
+            cHourEl.innerText = '00';
+            cMinEl.innerText = '00';
+            cSecEl.innerText = '00';
+            checkWork = true;
+            startOn = false;
             clearInterval(start);
         })
     },1000)
